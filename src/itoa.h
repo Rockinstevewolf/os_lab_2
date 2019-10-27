5a4c09843495f4fc40510515b515142d6ec3df04
Stepan Barannikov <SteveWolf1997@gmail.com>
2019-10-27 16:41:05 +0300

    reworked without using stdio.h lib
    no stdio

---
+++ src/itoa.h
@@ -0,0 +1,5 @@
+#include <stdlib.h>
+#include <string.h>
+
+void reverse(char *s);
+int itoa(int n, char *s);
--- src/main.c
+++ src/main.c
@@ -1,17 +1,18 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <unistd.h> 
 #include <sys/types.h> 
 #include <sys/wait.h>
-#include <string.h>
-//#include "CException.h>"
+#include <stdbool.h>
+#include "itoa.h"
 
 int fact(int a){
     if(a!=1){
         pid_t pid;
         int pipe_1[2], pipe_2[2], res=0, buff_a = a-1;
-        pipe(pipe_1);
-        pipe(pipe_2);
+        if((pipe(pipe_1) == -1) || (pipe(pipe_2) == -1)){
+        	write(STDERR_FILENO, "ERROR: pipe\n", sizeof "ERROR: pipe\n" - 1);
+        	exit(EXIT_FAILURE);
+        }
         pid = fork();
         if(pid > 0){    //Родитель
             close(pipe_1[0]);
@@ -22,6 +23,10 @@ int fact(int a){
             read(pipe_2[0], &res, sizeof(res));
             close(pipe_2[0]);
             return res+a;
+        }
+        else if(pid < 0){
+        	write(STDERR_FILENO, "ERROR: fork\n", sizeof "ERROR: fork\n" - 1);
+        	exit(EXIT_FAILURE);
         }
         else{   //Ребенок
             int get = 0;
@@ -41,25 +46,33 @@ int fact(int a){
 
 int main()
 {
-    int flag = 1;
-    int sol = 0;
-    int int_input = 0;
+    bool flag = true;
     int input;
-    printf("1. Чтобы вычислить сумму от 1 до n введите число\n");
-    printf("2. Чтобы завершить программу, введите '0'\n");
+    char save[100];
+    int save_len;
+    int sol = 0;
+    char sol_str[100];
+    int sol_str_len;
+    write(STDOUT_FILENO, "1. Чтобы вычислить сумму от 1 до n введите число\n", sizeof "1. Чтобы вычислить сумму от 1 до n введите число\n" - 1);
+    write(STDOUT_FILENO, "2. Чтобы завершить программу, введите '0'\n", sizeof "2. Чтобы завершить программу, введите '0'\n" - 1);
     while(flag){
-        printf("> ");
-        scanf("%d", &input);
+        write(STDOUT_FILENO, "> ", sizeof "> " - 1);
+        save_len = read(STDIN_FILENO, save, 100); //считывание команды
+        save[save_len-1] = '\0';
+       	input = atoi(save);
         if(input == 0){
-            flag = 0;
-            printf("Выход из программы...\n");
+            flag = false;
+            write(STDOUT_FILENO, "Выход из программы...\n", sizeof "Выход из программы...\n" - 1);
         }
         else{
             if(input < 0)
-            	printf("Ошибка, введено некорректное число!\n");
+            	write(STDOUT_FILENO, "Ошибка, введено некорректное число!\n", sizeof "Ошибка, введено некорректное число!\n" - 1);
             else if(input > 0){
                 sol = fact(input);
-                printf("Итоговая сумма = %d\n", sol);
+                sol_str_len = itoa(sol, sol_str);
+                write(STDOUT_FILENO, "Итоговая сумма = ", sizeof "Итоговая сумма = " - 1);
+            	write(STDOUT_FILENO, sol_str, sol_str_len);
+                write(STDOUT_FILENO, "\n", sizeof "\n" - 1);
             }
         }
     }
